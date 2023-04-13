02ecbe6f33a89dedf42a8dc226577e86a1f04163
ritvikshandilya <ritvikshandilya@gmail.com>
2023-04-13 03:55:14 +0200

    commit

--- Basic Scene/script.js
+++ Basic Scene/script.js
@@ -1,1 +1,8 @@
-console.log('hello three.js');
+console.log(THREE);
+//We need to create a scene
+    //scene is like a container
+    //we put object, models and lights etc in it
+    //at some point, we ask three.js to render that scene
+//creating scene
+
+const scene = new THREE.Scene()
