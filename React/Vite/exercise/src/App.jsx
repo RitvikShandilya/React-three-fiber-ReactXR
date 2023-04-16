e9a5a8c652ce7232427ee84d7619d8215212d324
ritvikshandilya <ritvikshandilya@gmail.com>
2023-04-16 23:40:20 +0200

    committed

--- React/Vite/exercise/src/App.jsx
+++ React/Vite/exercise/src/App.jsx
@@ -4,18 +4,24 @@ import Clicker from "./Clicker.jsx";
 export default function App() {
     
     const [hasClicker, setHasClicker] = useState(true);
-    
+    const [count, setCount] = useState(0);
+
     const toggleClicker = () => {
         setHasClicker(!hasClicker);
     }
 
+    const increment = () => {
+        setCount(count + 1);
+    } 
+
     return <>
+        <div>Total count: { count }</div>
         <button onClick = {toggleClicker}>{hasClicker ? 'Hide' : 'Show'} Clicker</button>
         { hasClicker ? <Clicker /> : null }
         {hasClicker && 
         <>
-            <Clicker color = "orange" keyName="CountA"/>
-            <Clicker color = "green" keyName="CountB"/>
+            <Clicker increment = { increment } color = "orange" keyName="CountA"/>
+            <Clicker increment = { increment } color = "green" keyName="CountB"/>
         </>
         }
     </>
